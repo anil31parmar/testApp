5598d5cc12333b859ebcf2d961de216e1b3ee782
anil31parmar <anil99parmar@gmail.com>
2022-08-23 17:45:08 +0530

    chages for firebase in home component

---
+++ src/app/Components/firebaseconfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDSdLA7TkQiE3ei5oi5TK5otPqnPBv5xVs",
+  authDomain: "testapp-da55c.firebaseapp.com",
+  projectId: "testapp-da55c",
+  storageBucket: "testapp-da55c.appspot.com",
+  messagingSenderId: "904589239960",
+  appId: "1:904589239960:web:328fac2d79f532732352ed"
+};
